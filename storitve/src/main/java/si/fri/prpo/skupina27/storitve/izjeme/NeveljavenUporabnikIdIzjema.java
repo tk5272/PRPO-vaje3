69bee7041585b6a3b1bedfd26ba218a071369c55
timotejkosir <timotej.kosir@gmail.com>
2020-12-07 22:15:34 +0100

    koncal vaje

--- storitve/src/main/java/si/fri/prpo/skupina27/storitve/izjeme/NeveljavenUporabnikIdIzjema.java
+++
@@ -1,7 +0,0 @@
-package si.fri.prpo.skupina27.storitve.izjeme;
-
-public class NeveljavenUporabnikIdIzjema extends RuntimeException {
-    public NeveljavenUporabnikIdIzjema(String msg) {
-        super(msg);
-    }
-}
